896d0b9d8b2d7493ffb648f63818e50177868b19
Vinicius Piovesan <viniciusmap22@gmail.com>
2024-10-31 23:11:47 -0300

    update1.1

--- src/config/bDados.js
+++ src/config/bDados.js
@@ -4,13 +4,13 @@ const mysql = require('mysql2');
 
 // Configurações do banco de dados usando variáveis de ambiente
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,         // Host do banco de dados
-    user: process.env.DB_USER,         // Usuário do banco de dados
-    password: process.env.DB_PASSWORD, // Senha do banco de dados
-    database: process.env.DB_NAME,     // Nome do banco de dados
-    port: process.env.DB_PORT || 3306, // Porta do banco de dados (padrão 3306)
+    host: process.env.DB_HOST,        
+    user: process.env.DB_USER,         
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_NAME,     
+    port: process.env.DB_PORT || 3306, 
     ssl: {
-        rejectUnauthorized: false      // Ignorar erros de SSL para Azure
+        rejectUnauthorized: false      
     }
 });
 
